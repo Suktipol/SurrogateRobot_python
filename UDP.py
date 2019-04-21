48a217b6fc76fec7da93d25b9e14e929a811690a
Suktipol <suktipol.kia@gmail.com>
2019-04-21 17:48:30 +0700

    remove unused file

--- UDP.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-UDP_IP_ADDRESS = "10.61.5.13"
-UDP_PORT_NO = 8052
-Message = bytes(DM.b, 'utf-8')
-
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-clientSock.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT_NO))
